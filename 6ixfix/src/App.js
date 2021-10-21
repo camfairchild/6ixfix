a225121e68185c0790abd3ce10e4578e922d13fd
camfairchild <cameron.fairchild@mail.utoronto.ca>
2021-10-21 10:37:06 -0400

    Created react app

---
+++ 6ixfix/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/images/6ixfix_logo_transparent.png" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
