532dcbcbb6eacefd61c59f0eca1340dfaf64fc5b
Cameron Fairchild <cameron@fairchild.dev>
2021-11-26 23:11:31 -0500

    added webpage routes

--- server.js
+++ server.js
@@ -346,6 +346,35 @@ app.delete('/api/profiles/:id', mongoChecker, async (req, res) => {
 })
 
 /*************************************************/
+
+/*************************************************/
+
+
+/*** Webpage routes below **********************************/
+// Serve the build
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+    // check for page routes that we expect in the frontend to provide correct status code.
+    const goodPageRoutes = [
+		"/", "/search", "/dashboard",
+		"/help", "/about", "/profile",
+		"/login", "/signup", "/admin",
+		"/messages"
+	];
+    if (!goodPageRoutes.includes(req.url)) {
+        // if url not in expected page routes, set status to 404.
+        res.status(404);
+    }
+
+    // send index.html
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+/*************************************************/
+
+
 // Express server listening...
 const port = process.env.PORT || 5000
 app.listen(port, () => {
