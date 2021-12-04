a0cdba9584411d8b3de25598d6f8ce6a11cb75ae
Cameron Fairchild <cameron@fairchild.dev>
2021-12-04 12:01:14 -0500

    Create admin.js

---
+++ routes/api/admin.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { Router } from 'express';
+const router = new Router();
+
+const log = console.log;
+
+import mongoChecker from '../../middleware/mongoose.js';
+import isLoggedIn from '../../middleware/loggedin.js'
+
+const Profile = mongoose.model('Profile');
+const User = mongoose.model('User');
+
+router.get('/', mongoChecker, isLoggedIn, async (req, res) => {
+    const user_id = req.user
+    try {
+    const user = await User.findById(user_id).lean()
+    const profile = await Profile.findOne({ userName: user.userName }).lean()
+    const isAdmin = (profile.type === 'Admin')
+    
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error })
+    }
+})
+
+export default router;
