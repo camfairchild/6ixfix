8817b65d1302af776aae3bd60a273accd5ba1478
Cameron Fairchild <cameron@fairchild.dev>
2021-12-04 12:14:24 -0500

    Update isAdmin.js

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,11 +1,19 @@
- const user_id = req.user
+const User = mongoose.model('User');
+const Profile = mongoose.model('Profile');
+
+export default async function isAdmin(req, res, next) {
+  const user_id = req.user
     try {
-    const user = await User.findById(user_id).lean()
-    const profile = await Profile.findOne({ userName: user.userName }).lean()
-    const isAdmin = (profile.userType === 'Admin')
-    
+     const user = await User.findById(user_id).lean()
+     const profile = await Profile.findOne({ userName: user.userName }).lean()
+     const isAdmin = ()
+     if (profile.userType === 'Admin') {
+      next();
+     } else {
+      return res.status(401).json({ message: "Not admin"})
+     }
     } catch (error) {
       console.log(error)
       res.status(500).json({ error })
     }
-
+}
